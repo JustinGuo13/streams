ebe54220a3bc8a7bdfd2d4f358a1162eec8c38e3
PengYangGuo <pyangguo@gmail.com>
2020-11-25 16:54:49 -0800

    Refactored StreamCreate to StreamForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route	 } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -16,7 +16,11 @@ const App = () => {
 					<Header />
 					<Route path="/" exact component={StreamList} />
 					<Route path="/streams/new" exact component={StreamCreate} />
-					<Route path="/streams/edit" exact component={StreamEdit} />
+					<Route
+						path="/streams/edit/:id"
+						exact
+						component={StreamEdit}
+					/>
 					<Route
 						path="/streams/delete"
 						exact
